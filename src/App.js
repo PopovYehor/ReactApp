947b6b1071629590f3d2ef14a11ea12ed8198b6c
PopovYehor <103197666+PopovYehor@users.noreply.github.com>
2022-08-05 14:33:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import {LeftMenu, activeMenu} from './components/leftMenu';
 
 function App() {
   return (
-    
     <div className='App'>
-    
     <Header title = "My Header" menuActive = {activeMenu}/>
     <div className = "container">
       <LeftMenu item ="Menu item"/>
